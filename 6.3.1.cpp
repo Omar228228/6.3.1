a1097a09a47e4e7b3299d68f8b69086245ce1cc5
Omar228228 <goblock542@gmail.com>
2021-11-17 22:31:51 +0300

    first commit

---
+++ 6.3.1.cpp
@@ -0,0 +1,84 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <time.h>
+
+using namespace std;
+
+void CreateArray(int* a, const int n, int Low, int High) {
+    for (int i = 0; i < n; i++) 
+    {
+        a[i] = Low + rand() % (High - Low + 1);
+    }
+}
+
+template<typename T>
+void CreateArray(T* a, const int n, int Low, int High) {
+    for (int i = 0; i < n; i++) 
+    {
+        a[i] = (T)Low + rand() % (High - Low + 1);
+    }
+}
+
+void PrintArray(int* a, const int n) {
+    for (int i = 0; i < n; i++) 
+    {
+        cout << setw(4) << a[i];
+    }
+    cout << endl;
+}
+
+template <typename T>
+void PrintArray(T* a, const int n) {
+    for (int i = 0; i < n; i++) 
+    {
+        cout << setw(4) << a[i];
+    }
+    cout << endl;
+}
+
+int Min(int* a, const int n) {
+    int min = a[0];
+    for (int i = 0; i < n; i++) 
+    {
+        if (a[i] < min)
+            min = a[i];
+    }
+    return min;
+}
+
+template <typename T>
+int Min(T* a, const int n) {
+    int min = a[0];
+    for (int i = 0; i < n; i++) 
+    {
+        if (a[i] < min)
+            min = a[i];
+    }
+    return min;
+}
+
+int main()
+{
+    srand((unsigned)time(NULL));
+
+    int n;
+
+    cout << "n = "; cin >> n;
+
+    int* a = new int[n];
+
+    int Low;
+
+    cout << "Low = "; cin >> Low;
+
+    int High;
+
+    cout << "High = "; cin >> High;
+
+    CreateArray(a, n, Low, High);
+
+    PrintArray(a, n);
+    cout << " min = " << Min(a, n) << endl;
+
+    return 0;
+}
---
+++ UnitTest6.3.1/UnitTest6.3.1.cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "C:\Users\Юра\source\repos\6.3.1\6.3.1.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest631
+{
+	TEST_CLASS(UnitTest631)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+
+
+
+		}
+	};
+}
